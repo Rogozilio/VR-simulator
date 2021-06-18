c57425fa18c099e6807e3dca99eea3147bfd4004
AlexSerg24 <ars13@tpu.ru>
2021-06-18 17:12:35 +0700

    Добавили сцену с Locomotion

---
+++ Assets/Scripts/VR_Controller.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VR_Controller : MonoBehaviour
+{
+    public float m_Sensitivity = 0.1f;
+    public float m_maxSpeed = 1.0f;
+
+    public SteamVR_Action_Boolean m_MovePress = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    private float m_Speed = 0.0f;
+
+    private CharacterController m_CharacterController = null;
+    private Transform m_CameraRig = null;
+    private Transform m_Head = null;
+
+    private void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        m_CameraRig = SteamVR_Render.Top().origin;
+        m_Head = SteamVR_Render.Top().head;
+    }
+
+    private void Update()
+    {
+        HandleHead();
+        HandleHight();
+        CalculateMovement();
+    }
+
+    private void HandleHead()
+    {
+        Vector3 oldPosition = m_CameraRig.position;
+        Quaternion oldRotation = m_CameraRig.rotation;
+
+        transform.eulerAngles = new Vector3(0.0f, m_Head.rotation.eulerAngles.y, 0.0f);
+
+        m_CameraRig.position = oldPosition;
+        m_CameraRig.rotation = oldRotation;
+    }
+
+    private void CalculateMovement()
+    {
+        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Vector3 movement = Vector3.zero;
+
+        if (m_MovePress.GetStateUp(SteamVR_Input_Sources.Any))
+            m_Speed = 0;
+
+        if (m_MovePress.state)
+        {
+            m_Speed += m_MoveValue.axis.y * m_Sensitivity;
+            m_Speed = Mathf.Clamp(m_Speed, -m_maxSpeed, m_maxSpeed);
+
+            movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+        }
+
+        m_CharacterController.Move(movement);
+    }
+
+    private void HandleHight()
+    {
+        float headHight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+        m_CharacterController.height = headHight;
+
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = m_CharacterController.height / 2;
+        newCenter.y += m_CharacterController.skinWidth;
+
+        newCenter.x = m_Head.localPosition.x;
+        newCenter.z = m_Head.localPosition.z;
+
+        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+
+        m_CharacterController.center = newCenter;
+    }
+}
