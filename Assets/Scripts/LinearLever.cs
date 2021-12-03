2756f3a56d8e1147a0e2802d72721664c23bf555
AlexSerg24 <ars13@tpu.ru>
2021-12-03 14:32:08 +0700

    Добавили звуков на крайние положения рычагов, добавили им визуальную часть ограничений движения, переделаны вдавливаемые кнопки, передеделана телепортационная сетка

--- Assets/Scripts/LinearLever.cs
+++ Assets/Scripts/LinearLever.cs
@@ -11,11 +11,24 @@ public class LinearLever : MonoBehaviour
     private bool canChangePressure = true;
     private float boundsDelta = 0.1f;
     private float rotationAngle = 5f;
+    private AudioSource sound;
+    private bool canPlaySound = false;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
         if (linearMapping.value <= 0f + boundsDelta)
         {
+            if (canPlaySound)
+            {
+                sound.Play();
+                canPlaySound = false;
+            }
+
             canChangePressure = true;
         }
         else if (linearMapping.value >= 1f - boundsDelta)
@@ -24,7 +37,18 @@ public class LinearLever : MonoBehaviour
             {
                 rotatorObject.targetAngle = rotatorObject.targetAngle + rotationAngle * directionCoef;
             }
+
+            if (canPlaySound)
+            {
+                sound.Play();
+                canPlaySound = false;
+            }
+
             canChangePressure = false;
+        }
+        else
+        {
+            canPlaySound = true;
         }
     }
 }
